e50e7a4d658a483430a6ea02eefa75aa71873ce7
Samuel Davies (1000048175) <DAVISS6@student.op.ac.nz>
2025-07-24 10:45:37 +1200

    Added Enemies Struct to separate file

---
+++ Enemies.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UberProject
+{
+    public struct Enemies
+    {
+        public string enemyName;
+        public int enemyHP;
+        public int enemyAttack;
+        public string enemyDesc;
+
+        public Enemies(string name, int hp, int attack, string desc)
+        {
+            this.enemyName = name;
+            this.enemyHP = hp;
+            this.enemyAttack = attack;
+            this.enemyDesc = desc;
+        }
+        public override string ToString()
+        {
+            return $"{enemyName} | HP: {enemyHP} | Attack: {enemyAttack} | Description: {enemyDesc}";
+        }
+
+    }
+}
